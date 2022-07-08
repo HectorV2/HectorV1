98d909323575f3ddd827b3c8d18db06c8c13dae0
HectorV2 <ganiriwand@gmail.com>
2022-07-09 00:01:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
     console.log(chalk.greenBright('Your token is correct'));
     await delay(1000);
     console.clear('');
-    console.log(chalk.cyanBright(`
+    console.log(chalk.cyanBright(chalk.bold`
 ██╗  ██╗███████╗ ██████╗████████╗ ██████╗ ██████╗
 ██║  ██║██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗
 ███████║█████╗  ██║        ██║   ██║   ██║██████╔╝
@@ -36,11 +36,11 @@ const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
 ╚═╝  ╚═╝╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
 1. Crown
 2. Trophy
-      `));
+      `)));
 
-     const pilih = rs.question(chalk.cyanBright(chalk.bold('1 or 2 :'));
-     const auth = rs.question(chalk.cyanBright(chalk.bold('[+] Enter Auth Code : '));
-     const time = rs.question(chalk.cyanBright(chalk.bold('[+] Enter Delay Max 1000 : '));
+     const pilih = rs.question(chalk.cyanBright(chalk.bold('1 or 2 :')));
+     const auth = rs.question(chalk.cyanBright(chalk.bold('[+] Enter Auth Code : ')));
+     const time = rs.question(chalk.cyanBright(chalk.bold('[+] Enter Delay Max 1000 : ')));
 
      if (pilih == 1) {
        while (true) {
@@ -59,7 +59,7 @@ const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
              `\r[ ${moment().format('HH:mm')} ] ${chalk.cyanBright(
                `${country}`
              )} | ${username} | ${crown} | ${trophy}`
-           );
+           ));
            await delay(time)
          } else if (result == 'BANNED') {
            console.log(chalk.redBright(`Your Account Has Been Banned`));
@@ -86,35 +86,7 @@ const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
              `\r[ ${moment().format('HH:mm')} ] ${chalk.cyanBright(
                `${country}`
              )} | ${username} | ${trophy}`
-           );
-           await delay(time)
-         } else if (result == 'BANNED') {
-           console.log(chalk.redBright(`Your Account Has Been Banned`));
-           break;
-         } else if (result == 'SERVER_ERROR') {
-           continue;
-         } else {
-           continue;
-         }
-       }
-     if (pilih == 1) {
-       while (true) {
-         var ronde = "3";
-         const result = await GoStumble(ronde, auth);
-         if (!result) {
-           console.log(chalk.redBright(`Auth Sudah Expired`));
-         } else if (result.includes('User')) {
-           const data = JSON.parse(result);
-           const username = data.User.Username;
-           const country = data.User.Country;
-           const trophy = data.User.SkillRating;
-           const crown = data.User.Crowns;
-
-           console.log(chalk.cyanBright(chalk.bold(
-             `\r[ ${moment().format('HH:mm')} ] ${chalk.cyanBright(
-               `${country}`
-             )} | ${username} | ${crown} | ${trophy}`
-           );
+           ));
            await delay(time)
          } else if (result == 'BANNED') {
            console.log(chalk.redBright(`Your Account Has Been Banned`));
