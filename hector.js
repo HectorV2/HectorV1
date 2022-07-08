020e58695ea92f3ec8133a1212adcbedd0fd30fc
HectorV2 <ganiriwand@gmail.com>
2022-07-09 00:48:49 +0700

    Update and rename index.js to hector.js

--- hector.js
+++ hector.js
@@ -4,7 +4,8 @@ import chalk from "chalk";
 import fetch from "node-fetch";
 import delay from "delay";
 
-const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
+const Hector = (ronde, auth) => 
+new Promise((resolve, reject) => {
 
 fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
     method: 'GET',
@@ -27,7 +28,7 @@ fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
   if (token == "1") {
     await delay(1000);
     console.clear('');
-    console.log(chalk.cyanBright(chalk.bold(`
+    console.log(chalk.blueBright(chalk.bold(`
 ██╗  ██╗███████╗ ██████╗████████╗ ██████╗ ██████╗
 ██║  ██║██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗
 ███████║█████╗  ██║        ██║   ██║   ██║██████╔╝
@@ -38,15 +39,20 @@ fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
 2. Trophy
 `)));
 
-     const pilih = rs.question(chalk.cyanBright(chalk.bold('Choose 1 or 2 : ')));
-     const auth = rs.question(chalk.cyanBright(chalk.bold('[+] Enter Auth Code : ')));
+     const pilih = rs.question(chalk.blueBright(chalk.bold('Choose 1 or 2 : ')));
+     const auth = rs.question(chalk.blueBright(chalk.bold('[+] Enter Auth Code : ')));
 
      if (pilih == 1) {
+
        while (true) {
+
          var ronde = "3";
-         const result = await GoStumble(ronde, auth);
+         const result = await Hector(ronde, auth);
          if (!result) {
+
            console.log(chalk.redBright(`Auth Sudah Expired`));
+           break;
+
          } else if (result.includes('User')) {
            const data = JSON.parse(result);
            const username = data.User.Username;
@@ -54,50 +60,62 @@ fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
            const trophy = data.User.SkillRating;
            const crown = data.User.Crowns;
 
-           console.log(chalk.bold(
-             `\r[ ${moment().format('HH:mm')} ] ${chalk.blueBright(
-               `${country}`
-             )} | ${username} | ${crown} | ${trophy}`
-           ));
+           console.log(chalk.bold(`\r[ ${moment().format('HH:mm')} ] ${chalk.blueBright(`${country}`)} | ${username} | ${crown} | ${trophy}`));
            await delay(1800);
+
          } else if (result == 'BANNED') {
+
            console.log(chalk.redBright(`Your Account Has Been Banned`));
            break;
+
          } else if (result == 'SERVER_ERROR') {
+
            continue;
+
          } else {
+
            continue;
          }
        }
+
      } else if (pilih == 2) {
+
        while (true) {
+
          var ronde = "2";
-         const result = await GoStumble(ronde, auth);
-         if (!result) {
+         const hasil = await Hector(ronde, auth);
+         if (!hasil) {
+
            console.log(chalk.redBright(`Auth Sudah Expired`));
+           break;
+
          } else if (result.includes('User')) {
            const data = JSON.parse(result);
            const username = data.User.Username;
            const country = data.User.Country;
            const trophy = data.User.SkillRating;
 
-           console.log(chalk.bold(
-             `\r[ ${moment().format('HH:mm')} ] ${chalk.blueBright(
-               `${country}`
-             )} | ${username} | ${trophy}`
-           ));
-           await delay(1800)
+           console.log(chalk.bold(`\r[ ${moment().format('HH:mm')} ] ${chalk.blueBright(`${country}`)} | ${username} | ${trophy}`));
+           await delay(1800);
+
          } else if (result == 'BANNED') {
+
            console.log(chalk.redBright(`Your Account Has Been Banned`));
            break;
+
          } else if (result == 'SERVER_ERROR') {
            continue;
+
          } else {
+
            continue;
          }
        }
      }
+
    } else {
+
      console.log(chalk.redBright(`The token you entered is wrong`));
    }
+
 })();
