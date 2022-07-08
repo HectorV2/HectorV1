d4bf4204587e528012b34c4cd2df2c917055d886
HectorV2 <ganiriwand@gmail.com>
2022-07-09 00:18:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ const chalk = require('chalk');
 const rs = require('readline-sync');
 const delay = require('delay');
 
-const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
-
-  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/${ronde}', {
-    headers: {
-      'authorization': auth
-    }
-  })
-    .then(res => res.text())
-    .then(data => {
-      resolve(data);
-    })
-    .catch(err => {
-      reject(err);
-    });
-
-});
-
+const GoStumble = (ronde, auth) => 
+  new Promise((resolve, reject) => {
+    fetch.get(
+      'http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/${ronde}', 
+      {
+        headers: {
+         'authorization': auth
+        },
+      }
+    )
+      .then(res => res.text())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+       console.log(err);
+      });
+  });
 (async () => {
   const token = rs.question(chalk.redBright(`[+] Input you token : `));
   if (token == "1") {
-    console.log(chalk.greenBright('Your token is correct'));
     await delay(1000);
     console.clear('');
     console.log(chalk.cyanBright(chalk.bold(`
@@ -34,15 +34,16 @@ const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
 ██╔══██║██╔══╝  ██║        ██║   ██║   ██║██╔══██╗
 ██║  ██║███████╗╚██████╗   ██║   ╚██████╔╝██║  ██║
 ╚═╝  ╚═╝╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+[?] Choose a Number :
 1. Crown
 2. Trophy
       `)));
 
-     const pilih = rs.question(chalk.cyanBright(chalk.bold('1 or 2 :')));
+     const choose = rs.question(chalk.cyanBright(chalk.bold('1 or 2 :')));
      const auth = rs.question(chalk.cyanBright(chalk.bold('[+] Enter Auth Code : ')));
      const time = rs.question(chalk.cyanBright(chalk.bold('[+] Enter Delay Max 1000 : ')));
 
-     if (pilih == 1) {
+     if (choose == 1) {
        while (true) {
          var ronde = "3";
          const result = await GoStumble(ronde, auth);
@@ -55,11 +56,11 @@ const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
            const trophy = data.User.SkillRating;
            const crown = data.User.Crowns;
 
-           console.log(chalk.cyanBright(chalk.bold(
-             `\r[ ${moment().format('HH:mm')} ] ${chalk.cyanBright(
+           console.log(chalk.bold(
+             `\r[ ${moment().format('HH:mm')} ] ${chalk.blueBright(
                `${country}`
              )} | ${username} | ${crown} | ${trophy}`
-           )));
+           ));
            await delay(time)
          } else if (result == 'BANNED') {
            console.log(chalk.redBright(`Your Account Has Been Banned`));
@@ -70,7 +71,7 @@ const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
            continue;
          }
        }
-     } else if (pilih == 2) {
+     } else if (choose == 2) {
        while (true) {
          var ronde = "2";
          const result = await GoStumble(ronde, auth);
@@ -82,11 +83,11 @@ const GoStumble = (ronde, auth) => new Promise((resolve, reject) => {
            const country = data.User.Country;
            const trophy = data.User.SkillRating;
 
-           console.log(chalk.cyanBright(chalk.bold(
-             `\r[ ${moment().format('HH:mm')} ] ${chalk.cyanBright(
+           console.log(chalk.bold(
+             `\r[ ${moment().format('HH:mm')} ] ${chalk.blueBright(
                `${country}`
              )} | ${username} | ${trophy}`
-           )));
+           ));
            await delay(time)
          } else if (result == 'BANNED') {
            console.log(chalk.redBright(`Your Account Has Been Banned`));
